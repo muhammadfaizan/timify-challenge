ce435d7150ea8a3e8517b47696448635a5bb2e71
Muhammad Faizan <muhammad.faizan@alibaba-inc.com>
2019-01-18 15:30:34 +0500

    modified api to clean previous data

--- controllers/resources.js
+++ controllers/resources.js
@@ -4,64 +4,46 @@ const { ObjectID } = require('mongodb')
 const v = require('../services/index').validator;
 const createAndUpdateResources = async (req, res, next) => {
     
-    const updateAllCollection = (model, docs) => {
+    const updateAllCollection = async (model, docs) => {
+        await model.collection.remove();
         const bulk = model.collection.initializeUnorderedBulkOp();
-        return Promise.all(docs.map(doc => {
-            let { id, ...updateDoc} = doc;
-            return bulk.find({ 
-                _id: ObjectID(id)
-            })
-            .upsert()
-            .replaceOne(updateDoc);
-        }))
-        .then(() => new Promise((resolve, reject) => {
-                bulk.execute((err, result) => {
-                  if (err) reject(err)
-                  else resolve(result)
-                });
-            })
-        )
+
+        docs.forEach((doc) => {
+            doc._id = ObjectID(doc._id)
+            bulk.insert(doc)
+        });
+        return bulk.execute();
     }
     try {
+        let roomAndDoctorValidator = v.array(v.object({
+            _id: v.mongoId,
+            name: v.string,
+            times: v.compose([
+                v.length(7),
+                v.array(
+                    v.optional(
+                        v.object({
+                            begin: v.timeString,
+                            end: v.timeString
+                        })
+                    )
+                )
+            ])
+        }))
         let validator = v.object({
-            rooms: v.array(v.object({
-                id: v.mongoId,
-                name: v.string,
-                times: v.compose([
-                    v.length(7),
-                    v.array(
-                        v.optional(
-                            v.object({
-                                begin: v.timeString,
-                                end: v.timeString
-                            })
-                        )
-                    )
-                ])
-            })),
-            doctors: v.array(v.object({
-                id: v.mongoId,
-                name: v.string,
-                times: v.compose([
-                    v.length(7),
-                    v.array(
-                        v.optional(
-                            v.object({
-                                begin: v.timeString,
-                                end: v.timeString
-                            })
-                        )
-                    )
-                ])
-            }))
+            rooms: roomAndDoctorValidator,
+            doctors: roomAndDoctorValidator
         })
         validator(req.body);
+        await db.db.dropDatabase();
         let roomsData = await Promise.all([
             updateAllCollection(Room,req.body.rooms),
             updateAllCollection(Doctor, req.body.doctors)
         ]);
         
-        res.send(roomsData);
+        res.send({
+            success: true
+        });
     }
     catch (err) {
         next(err);
